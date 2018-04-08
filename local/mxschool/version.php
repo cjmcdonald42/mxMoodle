b648c689f30606d90e94f6b6bba96de138b39598
jere99 <jrdegreeff@gmail.com>
2018-04-07 23:51:56 -0400

    update student database table

--- local/mxschool/version.php
+++ local/mxschool/version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mxschool';
-$plugin->version = 2018040402;
+$plugin->version = 2018040704;
 $plugin->release = 'v3.0';
 $plugin->requires = 2017111302;  // Moodle 3.4.2+
 $plugin->maturity = MATURITY_ALPHA;
