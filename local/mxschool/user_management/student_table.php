f86c00614b8e63cf2381157c8812ab9a285606c4
jere99 <jrdegreeff@gmail.com>
2018-06-06 12:05:26 -0400

    unimportant change

--- local/mxschool/user_management/student_table.php
+++ local/mxschool/user_management/student_table.php
@@ -112,11 +112,10 @@ class student_table extends local_mxschool_table {
      * Formats the actions column.
      */
     protected function col_actions($values) {
-        if ($this->type === 'parents') {
-            return $this->edit_icon('/local/mxschool/user_management/parent_edit.php', $values->id).$this->delete_icon($values->id);
-        } else {
-            return $this->edit_icon('/local/mxschool/user_management/student_edit.php', $values->id);
-        }
+        return ($this->type === 'parents'
+            ? $this->edit_icon('/local/mxschool/user_management/parent_edit.php', $values->id).$this->delete_icon($values->id)
+            : $this->edit_icon('/local/mxschool/user_management/student_edit.php', $values->id)
+        );
     }
 
 }
