d43c89b1aec9ad35d19b774daedb6199f8e9fce2
jere99 <jrdegreeff@gmail.com>
2018-08-01 10:53:16 -0400

    transportation manager block

---
+++ blocks/mxschool_manage_transportation/version.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Middlesex School's Transportation Manager Block for the Dashboard.
+ *
+ * @package    block_mxschool_manage_transportation
+ * @author     Jeremiah DeGreeff, Class of 2019 <jrdegreeff@mxschool.edu>
+ * @author     Charles J McDonald, Academic Technology Specialist <cjmcdonald@mxschool.edu>
+ * @copyright  2018, Middlesex School, 1400 Lowell Rd, Concord MA
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_mxschool_manage_transportation';
+$plugin->version = 2018073101;
+$plugin->release = 'v3.0';
+$plugin->requires = 2017111300; // Moodle 3.4+.
+$plugin->maturity = MATURITY_BETA;
+$plugin->dependencies = array('local_mxschool' => 2018073000, 'local_peertutoring' => 2018073000);
