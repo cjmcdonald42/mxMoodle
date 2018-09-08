709721d45dd457e88def6fdfd1d9b70661e82e64
jere99 <jrdegreeff@gmail.com>
2018-09-08 13:27:01 -0400

    fix for deleted weekend forms

--- local/mxschool/checkin/weekend_table.php
+++ local/mxschool/checkin/weekend_table.php
@@ -84,9 +84,11 @@ class weekend_table extends local_mxschool_table {
         $where = array(
             'u.deleted = 0', "s.boarding_status = 'Boarder'", $filter->dorm ? "s.dormid = {$filter->dorm}" : '',
             $filter->submitted === '1' ? "EXISTS (
-                SELECT userid FROM mdl_local_mxschool_weekend_form wf WHERE s.userid = userid AND wf.weekendid = {$filter->weekend}
+                SELECT userid FROM mdl_local_mxschool_weekend_form wf
+                WHERE s.userid = userid AND wf.active = 1 AND wf.weekendid = {$filter->weekend}
             )" : ($filter->submitted === '0' ? "NOT EXISTS (
-                SELECT userid FROM mdl_local_mxschool_weekend_form wf WHERE s.userid = userid AND wf.weekendid = {$filter->weekend}
+                SELECT userid FROM mdl_local_mxschool_weekend_form wf
+                WHERE s.userid = userid AND wf.active = 1 AND wf.weekendid = {$filter->weekend}
             )" : '')
         );
         $sortable = array('student', 'dorm', 'room', 'grade');
