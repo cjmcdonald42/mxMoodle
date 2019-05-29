dea63dfa7fec77145ff8cb64720b0765d1906bd1
jere99 <jrdegreeff@mxschool.edu>
2019-05-29 12:02:40 -0400

    Modify login page to generate login url dynamically

--- altLoginPage/login.php
+++ altLoginPage/login.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php require(__DIR__.'/../config.php'); ?>
 <html lang="en-US">
 <head>
   <title>Middlesex Moodle</title>
@@ -19,8 +20,8 @@
     <div class="w3-card-4 w3-margin">
       <div class="w3-container w3-highway-red">
         <h1 style="text-align:center"><i class="material-icons" style="font-size:36px">school</i> MX Moodle</h1>
-      </div>
-      <form class="w3-container" action="http://moodledev.mxschool.edu/moodle/login/index.php" method="post">
+    </div>
+      <form class="w3-container" action="<?php echo $CFG->wwwroot ?>/login/index.php" method="post">
         <p>Please sign in with your network credentials:</p>
         <input class="w3-input w3-border w3-round-large" type="text" name="username" placeholder="Username"><br>
         <input class="w3-input w3-border w3-round-large" type="password" name="password" placeholder="Password"><br><br>
@@ -36,7 +37,7 @@
       </div>
       <p class="w3-margin">You must sign in with your network credentials to access our Moodle site.
         You may use your username or your full email address and your network password.</p>
-      <p class="w3-margin">MX Moodle is owned by <a href="http://mxschool.edu/">Middlesex School</a>, an independent,
+      <p class="w3-margin">MX Moodle is owned by <a href="https://mxschool.edu/">Middlesex School</a>, an independent,
         non-denominational, residential, college-preparatory school that, for over one hundred years, has been committed
         to excellence in the intellectual, ethical, creative, and physical development of young people.
         If you have questions regarding our Moodle site, please connect with <a href="mailto:chuck@mxschool.edu">Chuck
