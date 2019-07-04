e620d91f70b0b692571bf7d0788f28b61b9854de
jere99 <jrdegreeff@mxschool.edu>
2019-07-04 17:56:06 -0400

    small things

--- local/peertutoring/settings.php
+++ local/peertutoring/settings.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Admin settings for Middlesex School's Dorm and Student functions plugin.
+ * Admin settings for Middlesex School's Peer Tutoring Subplugin.
  *
  * @package    local_peertutoring
  * @author     Jeremiah DeGreeff, Class of 2019 <jrdegreeff@mxschool.edu>
