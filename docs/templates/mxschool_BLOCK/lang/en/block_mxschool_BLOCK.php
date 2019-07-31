c54d9b8f633411f5a4be68e805241dcae0690d44
jere99 <jrdegreeff@mxschool.edu>
2019-07-31 11:15:38 -0400

    add block template

---
+++ docs/templates/mxschool_BLOCK/lang/en/block_mxschool_BLOCK.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English language strings for BLOCK_DESCRIPTION.
+ *
+ * @package    block_mxschool_BLOCK
+ * @author     Jeremiah DeGreeff, Class of 2019 <jrdegreeff@mxschool.edu>
+ * @author     Charles J McDonald, Academic Technology Specialist <cjmcdonald@mxschool.edu>
+ * @copyright  2019 Middlesex School, 1400 Lowell Rd, Concord MA 01742 All Rights Reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = // TODO: Plugin Name (This is the name when selecting the block to be added).
+$string['blockname'] = // TODO: Block Name (This is the header at the top of the block).
+$string['mxschool_BLOCK:addinstance'] = 'Middlesex School: Add PLUGIN_NAME';
+$string['mxschool_BLOCK:myaddinstance'] = 'Middlesex School: Add PLUGIN_NAME to Dashboard';
+
+// TODO: Any other language strings, such as all of the links.
