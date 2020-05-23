ae6dc934c4bd9f1fa09428b369002f21c212d910
Charles J McDonald <cjmcdonald@mxschool.edu>
2020-05-23 11:29:26 -0400

    Initialize Blocks

--- local/mxschool/healthpass/form.php
+++ local/mxschool/healthpass/form.php
@@ -24,6 +24,12 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+ require(__DIR__.'/../../../config.php');
+ require_once(__DIR__.'/../locallib.php');
+
+
+
+
 ***
 
 
@@ -37,114 +43,3 @@
 // Do you have a loss of smell or taste?
 // Do you have shortness of breath?
 // Submit button.
-
-
-
-
-
-
-
-
-
-
-require(__DIR__.'/../../../config.php');
-require_once(__DIR__.'/../locallib.php');
-
-require_login();
-$isstudent = user_is_student();
-if (!$isstudent) {
-    require_capability('local/mxschool:manage_advisor_selection', context_system::instance());
-}
-
-$id = optional_param('id', 0, PARAM_INT);
-
-setup_mxschool_page('form', 'advisor_selection');
-$PAGE->requires->js_call_amd('local_mxschool/advisor_selection_form', 'setup');
-
-$queryfields = array(
-    'local_mxschool_adv_selection' => array(
-        'abbreviation' => 'asf',
-        'fields' => array(
-            'id', 'userid' => 'student', 'keep_current', 'option1id' => 'option1', 'option2id' => 'option2',
-            'option3id' => 'option3', 'option4id' => 'option4', 'option5id' => 'option5', 'selectedid' => 'selected',
-            'time_created' => 'timecreated', 'time_modified' => 'timemodified'
-        )
-    )
-);
-
-if ($isstudent && !student_may_access_advisor_selection($USER->id)) {
-    redirect_to_fallback();
-}
-if ($id) { // Updating an existing record.
-    if (!$DB->record_exists('local_mxschool_adv_selection', array('id' => $id))) {
-        redirect_to_fallback();
-    }
-    $data = get_record($queryfields, "asf.id = ?", array($id));
-    if ($isstudent && $data->student !== $USER->id) { // Students can only edit their own forms.
-        redirect($PAGE->url);
-    }
-} else { // Creating a new record.
-    $data = new stdClass();
-    $data->id = $id;
-    $data->timecreated = time();
-    $data->keep_current = 1;
-    if ($isstudent) {
-        $existingid = $DB->get_field('local_mxschool_adv_selection', 'id', array('userid' => $USER->id));
-        if ($existingid) { // There can only be one advisor selection form per student.
-            redirect(new moodle_url($PAGE->url, array('id' => $existingid)));
-        }
-        $data->student = $USER->id;
-    }
-}
-$data->isstudent = $isstudent ? '1' : '0';
-$data->warning = get_config('local_mxschool', 'advisor_form_closing_warning');
-$data->instructions = get_config('local_mxschool', 'advisor_form_instructions');
-$students = get_student_with_advisor_form_enabled_list();
-$faculty = array(0 => get_string('form:select:default', 'local_mxschool')) + get_faculty_list();
-
-$form = new local_mxschool\local\advisor_selection\form(array('students' => $students, 'faculty' => $faculty));
-$form->set_data($data);
-
-if ($form->is_cancelled()) {
-    redirect($form->get_redirect());
-} else if ($data = $form->get_data()) {
-    $current = $DB->get_field('local_mxschool_student', 'advisorid', array('userid' => $data->student));
-    $data->timemodified = time();
-    if ($data->keep_current) {
-        $discardfrom = 1;
-    } else {
-        for ($i = 1; $i <= 5; $i++) {
-            if (!$data->{"option{$i}"}) {
-                $discardfrom = $i;
-                break;
-            }
-            if ($data->{"option{$i}"} === $current) {
-                $discardfrom = $i + 1;
-                break;
-            }
-        }
-    }
-    if (isset($discardfrom)) {
-        for ($i = $discardfrom; $i <= 5; $i++) {
-            $data->{"option{$i}"} = 0;
-        }
-    }
-    if (!isset($data->selected)) {
-        $data->selected = 0;
-    }
-    $id = update_record($queryfields, $data);
-    $result = (new local_mxschool\local\advisor_selection\submitted($id))->send();
-    logged_redirect(
-        $form->get_redirect(), get_string('advisor_selection:form:success', 'local_mxschool'), $data->id ? 'update' : 'create'
-    );
-}
-
-$output = $PAGE->get_renderer('local_mxschool');
-$renderable = new local_mxschool\output\form($form);
-
-echo $output->header();
-echo $output->heading(
-    $isstudent ? get_string('advisor_selection_form_title', 'local_mxschool', format_student_name($USER->id)) : $PAGE->title
-);
-echo $output->render($renderable);
-echo $output->footer();
