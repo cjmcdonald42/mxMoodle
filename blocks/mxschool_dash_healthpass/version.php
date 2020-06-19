e4ef13aa0c33b93531ae1f41e631e13d5735488a
mxChuck <cjmcdonald@mxschool.edu>
2020-06-19 16:48:51 -0400

    Updated Version

--- blocks/mxschool_dash_healthpass/version.php
+++ blocks/mxschool_dash_healthpass/version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_mxschool_dash_healthpass';
-$plugin->version = 2020061701;
+$plugin->version = 2020061900;
 $plugin->release = 'v3.2';
 $plugin->requires = 2019052000; // Moodle 3.7.
 $plugin->maturity = MATURITY_BETA;
