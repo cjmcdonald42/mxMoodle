2486fba5030bbac7426c913bc022cf3c90a60ffd
AaravMehta4897 <58570210+AaravMehta4897@users.noreply.github.com>
2021-07-16 10:50:00 -0400

    Update audit_table.php

--- local/mxschool/classes/local/healthtest/audit_table.php
+++ local/mxschool/classes/local/healthtest/audit_table.php
@@ -48,11 +48,11 @@
        parent::__construct('healthtest_audit_table', $columns, $headers, $sortable, $centered, $filter, true);
 
  	  // The fields to query from the database
-       $fields = array('ht.id AS htid' , 'u.lastname', 'u.firstname', 'u.alternatename', 'u.lastname AS name', 'tb.testing_cycle', 'tb.id AS tbid', 'tb.start_time',
+       $fields = array('u.id AS htid' , 'u.lastname', 'u.firstname', 'u.alternatename', 'u.lastname AS name', 'tb.testing_cycle', 'tb.id AS tbid', 'tb.start_time',
 					'tb.end_time', 'tb.date AS tbdate');
  	  // The tables which to query
        $from = array('{local_mxschool_healthtest} ht', '{local_mxschool_testing_block} tb ON tb.id = ht.testing_block_id',
-  					'{user} u ON u.id=ht.userid');
+  					'{user} u ON ht.userid=u.id');
  	  // Get everything unless there are filters
  	  $where = array('u.deleted = 0');
 
