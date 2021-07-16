b3a9b82c6d42230514d5b3067966539012d074fe
AaravMehta4897 <58570210+AaravMehta4897@users.noreply.github.com>
2021-07-16 08:11:55 -0400

    update

--- local/mxschool/classes/local/healthtest/audit_table.php
+++ local/mxschool/classes/local/healthtest/audit_table.php
@@ -52,11 +52,11 @@
        parent::__construct('healthtest_audit_table', $columns, $headers, $sortable, $centered, $filter, true);
 
  	  // The fields to query from the database
-       $fields = array('u.id' , 'u.lastname', 'u.firstname', 'u.alternatename', 'u.lastname AS name', 'tb.testing_cycle', 'tb.id AS tbid', 'tb.start_time',
+       $fields = array('u.id AS htid' , 'u.lastname', 'u.firstname', 'u.alternatename', 'u.lastname AS name', 'tb.testing_cycle', 'tb.id AS tbid', 'tb.start_time',
 					'tb.end_time', 'tb.date AS tbdate');
  	  // The tables which to query
        $from = array('{local_mxschool_healthtest} ht', '{local_mxschool_testing_block} tb ON tb.id = ht.testing_block_id',
-  					'{user} u'); // getting the ppl who have a testing cycle signed up but we want ppl who have access
+  					'{user} u');
  	  // Get everything unless there are filters
  	  $where = array('u.deleted = 0');
 
