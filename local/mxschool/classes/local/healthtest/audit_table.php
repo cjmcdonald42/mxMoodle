9d024fa701117e3f361468bca317c957850e94b9
AaravMehta4897 <58570210+AaravMehta4897@users.noreply.github.com>
2021-07-18 21:57:36 -0400

    oops-update

--- local/mxschool/classes/local/healthtest/audit_table.php
+++ local/mxschool/classes/local/healthtest/audit_table.php
@@ -49,7 +49,7 @@
 
 
  	  // The fields to query from the database
-       $fields = array('av.id AS userid' , 'u.lastname', 'u.firstname', 'u.alternatename', 'u.lastname AS name', 'av.testing_cycle', 'tb.id AS tbid', 'tb.start_time',
+       $fields = array('av.id AS userid' , 'u.lastname', 'u.firstname', 'u.alternatename', 'u.lastname AS name', 'tb.testing_cycle', 'tb.id AS tbid', 'tb.start_time',
 					'tb.end_time', 'tb.date AS tbdate');
  	  // The tables which to query
        $from = array('{local_mxschool_audit} av', '{user} u ON u.id = av.userid', '{local_mxschool_healthtest} ht ON u.id = ht.userid', '{local_mxschool_testing_block} tb ON tb.id = ht.testing_block_id',
