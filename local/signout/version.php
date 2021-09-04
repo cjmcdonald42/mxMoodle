b276517cd71eaa0f1b86c919a97afe46e39cda44
Charles J McDonald <cjmcdonald@mxschool.edu>
2021-09-04 14:27:57 -0400

    Fixed Magnus Permission Error

--- local/signout/version.php
+++ local/signout/version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_signout';
-$plugin->version = 2019090100;
+$plugin->version = 2021090402;
 $plugin->release = 'v3.1';
 $plugin->requires = 2019052000; // Moodle 3.7.
 $plugin->maturity = MATURITY_STABLE;
