369eea208edb2437f9836657d209ee972b5daa16
Charles J McDonald <cjmcdonald@mxschool.edu>
2021-09-05 09:25:31 -0400

    Function clean up

--- local/signout/version.php
+++ local/signout/version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_signout';
-$plugin->version = 2021090402;
+$plugin->version = 2021090500;
 $plugin->release = 'v3.1';
 $plugin->requires = 2019052000; // Moodle 3.7.
 $plugin->maturity = MATURITY_STABLE;
