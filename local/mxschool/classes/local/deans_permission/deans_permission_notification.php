f92f7fb3770adde2a4ba1e427ba083a39af96d8f
Charles J McDonald <cjmcdonald@mxschool.edu>
2022-07-06 09:48:47 -0400

    Update deans_permission_notification.php
    Add meta tag for internal_comment

--- local/mxschool/classes/local/deans_permission/deans_permission_notification.php
+++ local/mxschool/classes/local/deans_permission/deans_permission_notification.php
@@ -19,9 +19,8 @@
  *
  * @package     local_mxschool
  * @subpackage  deans_permission
- * @author      Cannon Caspar, Class of 2021 <cpcaspar@mxschool.edu>
- * @author      Charles J McDonald, Academic Technology Specialist <cjmcdonald@mxschool.edu>
- * @copyright   2020 Middlesex School, 1400 Lowell Rd, Concord MA 01742 All Rights Reserved.
+ * @author      Moodle Development Team
+ * @copyright   2022 Middlesex School, 1400 Lowell Rd, Concord MA 01742 All Rights Reserved.
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -38,41 +37,40 @@ abstract class deans_permission_notification extends \local_mxschool\notificatio
      * @throws coding_exception If the specified record does not exist.
      */
     public function __construct($emailclass, $id=0) {
-	    global $DB;
-         parent::__construct($emailclass);
+        global $DB;
+        parent::__construct($emailclass);
 
-         if ($id) {
-             $record = $DB->get_record_sql(
-                 "SELECT dp.id, CONCAT(u.firstname, ' ', u.lastname) AS fullname, su.grade, su.boarding_status, dpe.name AS event,
- 			 	    dp.sport, dp.times_away, dp.recurring, dp.external_comment, dp.event_info
- 			  FROM {local_mxschool_deans_perm} dp LEFT JOIN {user} u ON dp.userid = u.id
- 			  							   LEFT JOIN {local_mxschool_student} su ON dp.userid = su.userid
-										   LEFT JOIN {local_mxschool_dp_event} dpe ON dp.event_id = dpe.id
- 			  WHERE dp.id = {$id}"
-             );
-             if (!$record) {
-                 throw new \coding_exception("Record with id {$id} not found.");
-             }
+        if ($id) {
+            $record = $DB->get_record_sql(
+                "SELECT dp.id, CONCAT(u.firstname, ' ', u.lastname) AS fullname, su.grade, su.boarding_status, dpe.name AS event, dp.sport, dp.times_away, dp.recurring, dp.external_comment, dp.event_info, dp.internal_comment
+                FROM {local_mxschool_deans_perm} dp LEFT JOIN {user} u ON dp.userid = u.id
+                    LEFT JOIN {local_mxschool_student} su ON dp.userid = su.userid
+                    LEFT JOIN {local_mxschool_dp_event} dpe ON dp.event_id = dpe.id
+                    WHERE dp.id = {$id}"
+            );
+            if (!$record) {
+                throw new \coding_exception("Record with id {$id} not found.");
+            }
 
             $this->data['fullname'] = $record->fullname;
- 		  $this->data['grade'] = $record->grade;
- 		  $this->data['boarding_status'] = $record->boarding_status;
- 		  $this->data['event'] = $record->event;
- 		  $this->data['sport'] = $record->sport;
- 		  $this->data['times_away'] = $record->times_away;
-		  $this->data['recurring'] = $record->recurring;
-		  $this->data['message_to_student'] = $record->external_comment;
-		  $this->data['event_info'] = $record->event_info;
-         }
+            $this->data['grade'] = $record->grade;
+            $this->data['boarding_status'] = $record->boarding_status;
+            $this->data['event'] = $record->event;
+            $this->data['sport'] = $record->sport;
+            $this->data['times_away'] = $record->times_away;
+            $this->data['recurring'] = $record->recurring;
+            $this->data['internal_comment'] = $record->internal_comment;
+            $this->data['message_to_student'] = $record->external_comment;
+            $this->data['event_info'] = $record->event_info;
+        }
     }
 
-     /**
+    /**
       * @return array The list of strings which can serve as tags for the notification.
       */
-     public function get_tags() {
-         return array_merge(parent::get_tags(), array(
-             'fullname', 'grade', 'boarding_status', 'event', 'event_info', 'recurring', 'sport', 'times_away', 'message_to_student'
-         ));
-     }
-
+    public function get_tags() {
+        return array_merge(parent::get_tags(), array(
+            'fullname', 'grade', 'boarding_status', 'event', 'event_info', 'recurring', 'sport', 'times_away', 'message_to_student', 'internal_comment'
+        ));
+    }
 }
