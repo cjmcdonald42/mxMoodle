8c4f37d3fb6fc89a19538e516665bbfa824a8c3b
AaravMehta4897 <58570210+AaravMehta4897@users.noreply.github.com>
2022-07-13 16:52:05 -0400

    Lang File Update
    * Lang String Miss Study Hours is now Notify Head of House
    
    * Notify Dorm Log Email lang updates for preferences page

--- local/mxschool/lang/en/local_mxschool.php
+++ local/mxschool/lang/en/local_mxschool.php
@@ -1002,7 +1002,7 @@ $string['deans_permission:report:header:sport'] = 'Sport Played';
 $string['deans_permission:report:header:missing'] = 'Missing';
 $string['deans_permission:report:header:times_away'] = 'Dates';
 $string['deans_permission:report:header:parent_perm'] = 'Parent Permission?';
-$string['deans_permission:report:header:missing_studyhours'] = 'Miss Study Hours';
+$string['deans_permission:report:header:missing_studyhours'] = 'Notify Head of House';
 $string['deans_permission:report:header:sports_perm'] = 'Miss Sports';
 $string['deans_permission:report:header:class_perm'] = 'Miss Class';
 $string['deans_permission:report:header:internal_comment'] = 'Internal Comment';
@@ -1053,6 +1053,13 @@ $string['deans_permission:preferences:notify_student_email:note'] = 'If the mess
 $string['deans_permission:preferences:notify_student_email:notify_student_tags'] = 'Available tags for deans\' permission form notify student email';
 $string['deans_permission:preferences:notify_student_email:notify_student_subject'] = 'Deans\' permission form notify student email subject';
 $string['deans_permission:preferences:notify_student_email:notify_student_body'] = 'Deans\' permission form notify student email body';
+
+$string['deans_permission:preferences:notify_dorm_log_email'] = 'Deans\' Permission Form Notify Dorm Log Email Notification';
+$string['deans_permission:preferences:notify_dorm_log_email:info'] = 'Note:';
+$string['deans_permission:preferences:notify_dorm_log_email:note'] = 'If the message to student is sent, this email will go to the dorm log';
+$string['deans_permission:preferences:notify_dorm_log_email:notify_dorm_log_tags'] = 'Available tags for deans\' permission form notify dorm log email';
+$string['deans_permission:preferences:notify_dorm_log_email:notify_dorm_log_subject'] = 'Deans\' permission form notify dorm log email subject';
+$string['deans_permission:preferences:notify_dorm_log_email:notify_dorm_log_body'] = 'Deans\' permission form notify dorm log email body';
 
 // TODO Approved Notification Email
 $string['deans_permission:preferences:approved_email'] = 'Deans\' Permission Form Approved Email Notification';
