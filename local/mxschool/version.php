ccee2a4f3c5af146e2028277fdcbbb953e49c727
AaravMehta4897 <58570210+AaravMehta4897@users.noreply.github.com>
2022-07-13 16:31:07 -0400

    Update version.php

--- local/mxschool/version.php
+++ local/mxschool/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_mxschool';
-$plugin->version = 2022070502;
+$plugin->version = 20220706089;
 $plugin->release = 'v3.2';
 $plugin->requires = 2019052000; // Moodle 3.7.
 $plugin->maturity = MATURITY_STABLE;
